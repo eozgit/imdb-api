348e34c502fa8fdfbc986795989cc633276ea199
Enis Ozgit <eozgit@gmail.com>
2021-01-08 21:33:37 +0000

    Add mongo import script

--- dataset/download.sh
+++ dataset/download.sh
@@ -1,3 +1,7 @@
 cd /workspace/dataset
 wget -i list.txt
 gzip -d *.gz
+mongoimport --type tsv --db imdb --headerline --collection basics --file dataset/title.basics.tsv
+mongoimport --type tsv --db imdb --headerline --collection ratings --file dataset/title.ratings.tsv
+mongoimport --type tsv --db imdb --headerline --collection principals --file dataset/title.principals.tsv
+mongoimport --type tsv --db imdb --headerline --collection names --file dataset/name.basics.tsv

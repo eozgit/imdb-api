98ef0b0de0fd00ad424d565d1fd07c56df5980b2
Enis Ozgit <eozgit@gmail.com>
2021-01-17 11:59:16 +0000

    Test actors service

--- src/actors/actors.controller.spec.ts
+++ src/actors/actors.controller.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { ActorsController } from './actors.controller';
 import { ActorsService } from './actors.service';
+import { MockActorRepository } from './actors.service.spec';
+import { Actor } from './entities/actor.entity';
 
 describe('ActorsController', () => {
   let controller: ActorsController;
@@ -8,7 +11,11 @@ describe('ActorsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ActorsController],
-      providers: [ActorsService],
+      providers: [ActorsService,
+        {
+          provide: getRepositoryToken(Actor),
+          useValue: MockActorRepository,
+        }],
     }).compile();
 
     controller = module.get<ActorsController>(ActorsController);
--- src/actors/actors.service.spec.ts
+++ src/actors/actors.service.spec.ts
@@ -1,18 +1,79 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { ObjectId } from 'mongodb';
 import { ActorsService } from './actors.service';
+import { Actor } from './entities/actor.entity';
+
+export const getMockActor = () => ({
+  _id: new ObjectId("5ff8cdd7e65563629d215c65"),
+  nconst: "nm0000011",
+  primaryName: "Gary Cooper",
+  birthYear: 1901,
+  deathYear: 1961,
+  primaryProfession: "actor,soundtrack,producer",
+  knownForTitles: "tt0027996,tt0035896,tt0044706,tt0034167"
+});
+
+export const getMockUpdatedActor = () => ({
+  _id: new ObjectId("5ff8cdd7e65563629d215c65"),
+  nconst: "nm0000011",
+  primaryName: "Gary Cooper",
+  birthYear: 1901,
+  deathYear: 1961,
+  primaryProfession: "actor",
+  knownForTitles: "tt0027996,tt0035896,tt0044706,tt0034167"
+});
+
+export const MockActorRepository = {
+  insertOne: jest.fn(() => getMockActor()),
+  find: jest.fn(() => [getMockActor()]),
+  findOneOrFail: jest.fn(() => getMockActor()),
+  save: jest.fn(() => ([getMockUpdatedActor()])),
+  deleteOne: jest.fn(() => null)
+};
 
 describe('ActorsService', () => {
   let service: ActorsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ActorsService],
+      providers: [ActorsService,
+        {
+          provide: getRepositoryToken(Actor),
+          useValue: MockActorRepository,
+        }],
     }).compile();
 
     service = module.get<ActorsService>(ActorsService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should insert and return the actor when create is called', async () => {
+    const actor = await service.create(getMockActor());
+
+    expect(actor).toEqual(getMockActor());
+  });
+
+  it('should retrieve and return matching actors when findAll is called', async () => {
+    const actor = await service.findAll(0, 'Gary', 1901);
+
+    expect(actor).toEqual([getMockActor()]);
+  });
+
+  it('should retrieve and return an actor record when findOne is called', async () => {
+    const actor = await service.findOne('nm0000011');
+
+    expect(actor).toEqual(getMockActor());
+  });
+
+  it('should update the profession and return updated values when update is called', async () => {
+    const actor = await service.update('tt0000007', { primaryProfession: 'Actor' });
+
+    expect(actor).toEqual(getMockUpdatedActor());
+  });
+
+  it('should remove the actor record and return null when remove is called', async () => {
+    const actor = await service.remove('nm0000011');
+
+    expect(actor).toEqual(undefined);
   });
 });

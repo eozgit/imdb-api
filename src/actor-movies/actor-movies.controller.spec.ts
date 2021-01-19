5888cb69df79f1f9693bf3cc4e51dc6321f7e46b
Enis Ozgit <eozgit@gmail.com>
2021-01-19 20:41:02 +0000

    Test actor-movie controller

--- src/actor-movies/actor-movies.controller.spec.ts
+++ src/actor-movies/actor-movies.controller.spec.ts
@@ -2,7 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { ActorMoviesController } from './actor-movies.controller';
 import { ActorMoviesService } from './actor-movies.service';
-import { MockActorMovieRepository } from './actor-movies.service.spec';
+import { getMockActorMovie, getMockUpdatedActorMovie, MockActorMovieRepository } from './actor-movies.service.spec';
 import { ActorMovie } from './entities/actor-movie.entity';
 
 describe('ActorMoviesController', () => {
@@ -21,7 +21,27 @@ describe('ActorMoviesController', () => {
     controller = module.get<ActorMoviesController>(ActorMoviesController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should insert and return the actor-movie record when create is called', async () => {
+    const actorMovie = await controller.create(getMockActorMovie());
+
+    expect(actorMovie).toEqual(getMockActorMovie());
+  });
+
+  it('should retrieve and return an actor-movie record when findOne is called', async () => {
+    const actorMovie = await controller.findOne('tt0000003', 'nm0721526');
+
+    expect(actorMovie).toEqual(getMockActorMovie());
+  });
+
+  it('should update the category and return updated values when update is called', async () => {
+    const actorMovie = await controller.update('tt0000003', 'nm0721526', { category: 'actor' });
+
+    expect(actorMovie).toEqual(getMockUpdatedActorMovie());
+  });
+
+  it('should remove the actor-movie record and return null when remove is called', async () => {
+    const actorMovie = await controller.remove('tt0000003', 'nm0721526');
+
+    expect(actorMovie).toEqual(undefined);
   });
 });
